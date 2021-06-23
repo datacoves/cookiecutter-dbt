12e79b10844e83a77a471b09c0f8890144c70999
Sebastian Sassi <ssassi@sophilabs.com>
2021-06-23 15:30:48 -0300

    Fix gitlab ci script.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,27 +1,18 @@
 
-
-TERMINATOR = "\x1b[0m"
-WARNING = "\x1b[1;33m [WARNING]: "
-INFO = "\x1b[1;33m [INFO]: "
-HINT = "\x1b[3;33m"
-SUCCESS = "\x1b[1;32m [SUCCESS]: "
-
-DEBUG_VALUE = "debug"
-
 def gitlab_ci_enabled(adapter):
     if adapter == "snowflake":
         print(
-            "To run your transformations, set the following environment variables in your GitLab project:\n"
+            "Set the following environment variables in your GitLab project to run dbt as part of your CI pipeline:\n"
             " - DBT_ACCOUNT: Your snowflake account.\n"
             " - DBT_USER: Your snowflake username\n"
             " - DBT_PASSWORD: Your snowflake password\n"
             " - TEST_DATABASE: The Database name you use for running CI jobs\n\n"
-            "Learn more here: https://docs.gitlab.com/ee/ci/variables/"
+            "Learn more about GitLab CI/CD variables here: https://docs.gitlab.com/ee/ci/variables/"
         )
 
 
 def main():
-    print(SUCCESS + "Project initialized successfully!" + TERMINATOR)
+    print("\nProject initialized successfully!\n")
     
     adapter = "{{ cookiecutter.adapter }}"
 
