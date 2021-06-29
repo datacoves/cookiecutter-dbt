2bc3bee796bf379387db4cde1cbe281873d3e1fd
Noel Gomez <noel_gomez@yahoo.com>
2021-06-28 17:59:22 -0700

    separate user and ci profiles, initialize git repo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,14 @@
+from pathlib import Path
+import os
+
+PROJECT_DIRECTORY = Path().resolve()
+def move_user_profiles():
+  source_file = PROJECT_DIRECTORY.joinpath('.config/user_profiles.yml')
+  target_file = PROJECT_DIRECTORY.joinpath('profiles.yml')
+  Path(source_file).rename(target_file)
+
+def initialize_git_repo():
+  os.system("git init")
 
 def post_init_messages(adapter, ci_tool):
 
@@ -12,14 +23,14 @@ def post_init_messages(adapter, ci_tool):
 
   if ci_tool == "None":
     message = (
-      f"A base profile.yml file has been created in folder .config\n"
+      f"A base profile.yml file has been created in folder your project folder\n"
       f"Copy it to ~/.dbt and edit its contents\n"
       f"For more information see https://docs.getdbt.com/dbt-cli/configure-your-profile \n\n"
     )
   else:
     message = (
-      f"A base profile.yml file has been created in the .config folder\n"
-      f"You will need to copy it to ~/.dbt and then edit file for you local development\n"
+      f"A base profile.yml file has been created in folder your project folder\n"
+      f"Copy it to ~/.dbt and edit its contents for local development\n"
       f"For more information see https://docs.getdbt.com/dbt-cli/configure-your-profile \n\n"
 
       f"The CI job will leverage the profile.yml file in the .config folder.\n"
@@ -27,13 +38,14 @@ def post_init_messages(adapter, ci_tool):
       f"project to run dbt as part of your CI pipeline.\n"
       f"Learn more about {ci_tool} CI/CD variables here: {docs}"
     )
-
   print(message)
 
 def main():
     adapter = "{{ cookiecutter.adapter }}"
     ci_tool = "{{ cookiecutter.ci_tool }}"
 
+    move_user_profiles()
+    initialize_git_repo()
     post_init_messages(adapter, ci_tool)
 
 if __name__ == "__main__":
