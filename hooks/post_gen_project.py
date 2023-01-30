da20e847f75498184bfa45a29edb6a94e72dc1fc
Aaron White <aaroncwhite@gmail.com>
2023-01-30 16:56:23 -0500

    refactor: we just use circleci for now

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -27,31 +27,21 @@ def init_env(run_env_creation, environment_manager):
     
 
 def post_init_messages(adapter, ci_tool):
-    if ci_tool == "GitHub":
-        docs = "https://docs.github.com/en/actions/reference/environment-variables"
-    elif ci_tool == "GitLab":
-        docs = "https://docs.gitlab.com/ee/ci/variables/"
 
     print(
         "\nProject initialized successfully!\n"
     )
+    ci_tool = 'CircleCI'
+    docs = 'https://circleci.com/docs/env-vars/'
+    message = (
+        f"A base profiles.yml file has been created in your project folder\n"
+        f"Copy it to ~/.dbt and edit its contents for local development\n"
+        f"For more information see https://docs.getdbt.com/dbt-cli/configure-your-profile \n\n"
 
-    if ci_tool == "None":
-        message = (
-            f"A base profiles.yml file has been created in folder your project folder\n"
-            f"Copy it to ~/.dbt and edit its contents\n"
-            f"For more information see https://docs.getdbt.com/dbt-cli/configure-your-profile \n\n"
-        )
-    else:
-        message = (
-            f"A base profiles.yml file has been created in your project folder\n"
-            f"Copy it to ~/.dbt and edit its contents for local development\n"
-            f"For more information see https://docs.getdbt.com/dbt-cli/configure-your-profile \n\n"
-
-            f"The CI job will leverage the profiles.yml file in the .config folder.\n"
-            f"You will need to set the corresponding environment variables in the {ci_tool}\n"
-            f"project to run dbt as part of your CI pipeline.\n"
-            f"Learn more about {ci_tool} CI/CD variables here: {docs}"
+        f"The CI job will leverage the profiles.yml file in the .circleci/dbt_config folder.\n"
+        f"You will need to set the corresponding environment variables in {ci_tool}\n"
+        f"project to run dbt as part of your CI pipeline.\n"
+        f"Learn more about {ci_tool} CI/CD variables here: {docs}"
         )
     print(message)
 
